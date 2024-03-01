34ff8713a9fedaf2cfd324cdf311585470c88d5b
Tamilarasan S <151763918+sktamil07@users.noreply.github.com>
2024-03-01 20:11:31 +0530

    Add files via upload
    Java file added

---
+++ sample.java
@@ -0,0 +1,87 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+		String a="Hello World";
+		System.out.println(a.substring(2,6));
+		/*String s1="Hello";
+		String s2="Hello";
+		System.out.print(s1.equals(s2));
+		
+		String s1="Hello";
+		String s2=new String("Hello");
+		System.out.print(s1==s2);
+		
+		int []a= {1,2,3,4,5};
+		int sum=0;
+		for(int i:a) {
+			if( i%2==0) {
+				continue;
+			}
+			sum+=i;
+			
+		}
+		System.out.print(sum);
+		
+		String s ="Hello";
+		System.out.println(s.indexOf("l"));
+		
+		int x=5;
+		int y=2;
+	System.out.println(x++ + ++y);
+	
+		int[]n= {1,2,3,4,5};
+		int sum=0;
+		for(int i=0;i<n.length;i++) {
+			sum+=n[i];
+		}
+		System.out.println(sum);
+		int x=10;
+		int y=5;
+		int r=(x+=y)*2;
+		System.out.print(r);
+		
+		
+		
+		
+		int x=10;
+		int y=5;
+		System.out.println(x>y&&y>0);
+		
+		int x=5;
+		System.out.println(x>3?"yes":"no");
+		
+		
+	    int x=5;
+		int y=10;
+		int z=15;
+		System.out.println(x<y?y<z?z:y:x);
+	
+		int a=5;
+		System.out.println(a>2?a<4?10:8:7);
+		
+		int a=10;
+		System.out.println(a++ + ++a - a--);
+		System.out.println(a);
+		System.out.println(a++ + 5);
+		System.out.println(a);
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a++ +"+"+ ++a);
+		System.out.println(a);
+		
+		
+		
+		
+		
+		int b=5;
+		System.out.print(b<<2);
+		*/
+		
+		int c=5;
+		int d=2;
+		System.out.print(c&d);
+		
+		}
+
+}
